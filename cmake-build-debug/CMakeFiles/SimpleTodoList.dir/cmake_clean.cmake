2a8f14a0c3340b2732939983eecceba6f15aaf50
aaronVerones <aaroninyvr@gmail.com>
2017-05-07 09:07:36 -0700

    Added .idea, cmake-build-debug, CMakeLists

---
+++ cmake-build-debug/CMakeFiles/SimpleTodoList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleTodoList.dir/main.cpp.obj"
+  "SimpleTodoList.pdb"
+  "SimpleTodoList.exe"
+  "SimpleTodoList.exe.manifest"
+  "libSimpleTodoList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleTodoList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
